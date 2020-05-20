6492e103b9bc965967f7830421b8be72ddf0938f
kataoka029 <kataoka029@gmail.com>
2020-05-21 00:13:39 +0900

    Connect to heroku database

--- bot.js
+++ bot.js
@@ -1,12 +1,14 @@
+// BASIC SETTING
 const express = require("express");
 const line = require("@line/bot-sdk");
 const dotenv = require("dotenv");
-
 dotenv.config();
 const config = {
   channelSecret: process.env.SECRET_KEY,
   channelAccessToken: process.env.ACCESS_TOKEN,
 };
+
+// BOT
 const client = new line.Client(config);
 
 const handleEvent = async (event) => {
@@ -21,7 +23,7 @@ const handleEvent = async (event) => {
 
 const webhookRouter = express.Router();
 webhookRouter.post("/", line.middleware(config), (req, res) => {
-  console.log(req.body.source); // can see by "heroku logs --tail"
+  console.log(req.body.events);
   Promise.all(req.body.events.map(handleEvent)).then((result) =>
     res.json(result)
   );
--- server.js
+++ server.js
@@ -1,14 +1,35 @@
+// BASIC SETTING
 const express = require("express");
 const { webhookRouter } = require("./bot.js");
 const morgan = require("morgan");
-
+const { Client } = require("pg");
+const dotenv = require("dotenv");
 const app = express();
 const PORT = process.env.PORT || 3000;
+dotenv.config();
 
+// DB
+const client = new Client({
+  database: "dbniuolm51dam7",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: "ec2-54-175-117-212.compute-1.amazonaws.com",
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+});
+client.connect();
+client.query("SELECT * FROM messages", (err, res) => {
+  console.log(err, res);
+  client.end();
+});
+
+// WEBHOOK
 app.use(morgan("dev"));
 app.use("/webhook", webhookRouter);
 
-app.get("/", (req, res) => res.send("Hello LINE BOT!!"));
+// DISPLAY
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.render("./index.ejs"));
 
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
