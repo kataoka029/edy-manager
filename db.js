484ec5be6e3328991ac8665bb2de0ae68c274807
kataoka029 <kataoka029@gmail.com>
2020-05-21 08:56:03 +0900

    Create db.js

---
+++ db.js
@@ -0,0 +1,11 @@
+const { Client } = require("pg");
+const client = new Client({
+  database: "dbniuolm51dam7",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  host: "ec2-54-175-117-212.compute-1.amazonaws.com",
+  port: 5432,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = { client };
--- server.js
+++ server.js
@@ -1,35 +1,28 @@
-// BASIC SETTING
+// SERVER
 const express = require("express");
-const { webhookRouter } = require("./bot.js");
 const morgan = require("morgan");
-const { Client } = require("pg");
 const dotenv = require("dotenv");
 const app = express();
-const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 // DB
-const client = new Client({
-  database: "dbniuolm51dam7",
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: "ec2-54-175-117-212.compute-1.amazonaws.com",
-  port: 5432,
-  ssl: { rejectUnauthorized: false },
-});
+const { client } = require("./db.js");
 client.connect();
 client.query("SELECT * FROM messages", (err, res) => {
-  console.log(err, res);
+  console.log(err, res.rows);
   client.end();
 });
 
 // WEBHOOK
+const { webhookRouter } = require("./bot.js");
 app.use(morgan("dev"));
 app.use("/webhook", webhookRouter);
 
-// DISPLAY
+// ROUTING
 app.set("view engine", "ejs");
 app.get("/", (req, res) => res.render("./index.ejs"));
 
+// START
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
