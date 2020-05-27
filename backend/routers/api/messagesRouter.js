c1044ae0f26c2c0924e03b6fed8e4b8b0e9889eb
kataoka029 <kataoka029@gmail.com>
2020-05-27 22:24:49 +0900

    Turn into frontend app

--- backend/routers/api/messagesRouter.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const getMessages = require("../../models/messages/get.js");
-const createMessage = require("../../models/messages/create.js");
-
-const messagesRouter = express.Router();
-messagesRouter.get("/", getMessages);
-messagesRouter.post("/", createMessage);
-
-module.exports = messagesRouter;
