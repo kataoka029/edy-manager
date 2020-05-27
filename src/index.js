8787789fe637bbaa061ee194a7c0fdfc34d6a393
kataoka029 <kataoka029@gmail.com>
2020-05-27 23:39:57 +0900

    Set react and webpack

--- src/index.js
+++ src/index.js
@@ -1,24 +1,34 @@
+// OLD
 const messagesContainer = document.querySelector("div.messages-container");
 const edyInputText = document.querySelector("div.edy-input div.text");
 const replyButton = document.querySelector("a.reply");
 const url = "https://c8bce5b3.ngrok.io/";
 
+// NEW
+import "./reset.css";
+import "./style.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App.js";
+ReactDOM.render(<App />, messagesContainer);
+
+// OLD
 // あるユーザーIDのメッセージを取得する
-fetch(`${url}/api/messages?u=1`)
-  .then((res) => res.json())
-  .then((messages) => {
-    for (const message of messages) {
-      const div = document.createElement("div");
-      div.classList.add("chat");
-      if (message.line_user_type === "user") {
-        div.classList.add("left-chat");
-      } else {
-        div.classList.add("right-chat");
-      }
-      div.innerHTML = message.line_message_text;
-      messagesContainer.appendChild(div);
-    }
-  });
+// fetch(`${url}/api/messages?u=1`)
+//   .then((res) => res.json())
+//   .then((messages) => {
+//     for (const message of messages) {
+//       const div = document.createElement("div");
+//       div.classList.add("chat");
+//       if (message.line_user_type === "user") {
+//         div.classList.add("left-chat");
+//       } else {
+//         div.classList.add("right-chat");
+//       }
+//       div.innerHTML = message.line_message_text;
+//       messagesContainer.appendChild(div);
+//     }
+//   });
 
 // DBには入るけど、LINEには送れていない
 replyButton.addEventListener("click", () => {
