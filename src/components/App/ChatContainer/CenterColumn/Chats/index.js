a0c2b0950ffaa5bc1955972cf7177e7fbe123eda
kataoka029 <kataoka029@gmail.com>
2020-05-28 23:55:00 +0900

    Adjust design

--- src/components/App/ChatContainer/CenterColumn/Chats/index.js
+++ src/components/App/ChatContainer/CenterColumn/Chats/index.js
@@ -3,13 +3,9 @@ import { useSelector, useDispatch } from "react-redux";
 import "./style.scss";
 const url = "https://edy-api.herokuapp.com/";
 
-//
-
-// // あるユーザーIDのメッセージを取得する
-
-const listChats = () => {
-  const chats = useSelector((state) => state.chats);
+const Chats = () => {
   const dispatch = useDispatch();
+  const chats = useSelector((state) => state.chats);
 
   fetch(`${url}/api/messages?u=1`)
     .then((res) => res.json())
@@ -19,32 +15,13 @@ const listChats = () => {
         chats: data,
       });
     });
+
   return chats.map((chat, index) => {
     console.log(chat);
     const className =
       chat.line_user_type === "user" ? "left-chat" : "right-chat";
-    return (
-      <div className="chat" key={`chat${index}`}>
-        <div className={className}>{chat.line_message_text}</div>
-      </div>
-    );
+    return <div className={`chat ${className}`}>{chat.line_message_text}</div>;
   });
 };
 
-const Chats = () => {
-  return listChats();
-};
-
 export default Chats;
-
-// for (const message of messages) {
-//   const div = document.createElement("div");
-//   div.classList.add("chat");
-//   if (message.line_user_type === "user") {
-//     div.classList.add("left-chat");
-//   } else {
-//     div.classList.add("right-chat");
-//   }
-//   div.innerHTML = message.line_message_text;
-//   messagesContainer.appendChild(div);
-// }
--- src/components/App/ChatContainer/CenterColumn/EdyInput/index.js
+++ src/components/App/ChatContainer/CenterColumn/EdyInput/index.js
@@ -3,26 +3,23 @@ import "./style.scss";
 
 const EdyInput = () => {
   return (
-    <div>
-      <h1>this is edyinput!!</h1>
-      <div className="edy-input">
-        <div className="menu-bar">
-          <a className="mood">
-            <span className="material-icons">mood</span>
-          </a>
-          <a className="attachment">
-            <span className="material-icons">attachment</span>
-          </a>
-          <a className="reply">
-            <span className="material-icons">reply</span>
-          </a>
-        </div>
-        <div
-          className="text"
-          data-placeholder="メッセージを入力"
-          contentEditable="true"
-        ></div>
+    <div className="edy-input">
+      <div className="menu-bar">
+        <a className="mood">
+          <span className="material-icons">mood</span>
+        </a>
+        <a className="attachment">
+          <span className="material-icons">attachment</span>
+        </a>
+        <a className="reply">
+          <span className="material-icons">reply</span>
+        </a>
       </div>
+      <div
+        className="text"
+        data-placeholder="メッセージを入力"
+        contentEditable="true"
+      ></div>
     </div>
   );
 };
