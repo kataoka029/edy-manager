6a0d23ef55d83ac5c7b54d30cb774048e6ea9174
kataoka029 <kataoka029@gmail.com>
2020-06-12 23:12:14 +0900

    Add web server on frontend side

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+app.get("/", (req, res) => res.render("index.html"));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
+console.log(`Server running at ${PORT}`);
