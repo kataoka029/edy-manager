aa9ce242e07788f427dda1d827d1246053f297c0
kataoka029 <kataoka029@gmail.com>
2020-07-07 21:49:04 +0900

    Display part of images

--- src/components/App/MessageContainer/CenterColumn/Messages/Message/index.js
+++ src/components/App/MessageContainer/CenterColumn/Messages/Message/index.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./style.scss";
+import config from "../../../../../../config";
+const url = config.url;
 
 const Message = (props) => {
   const className =
     props.message.line_user_type === "user" ? "left-message" : "right-message";
+  const [imgUrl, setImgUrl] = useState("");
+
+  useEffect(() => {
+    fetch(`${url}api/messages/${props.message.line_message_id}/image`)
+      .then((res) => res.body)
+      .then((stream) => new Response(stream))
+      .then((response) => response.blob())
+      .then((blob) => URL.createObjectURL(blob))
+      .then((url) => setImgUrl(url));
+  }, []);
+
   switch (props.message.line_message_type) {
     case "image":
       return (
         <div className={`message ${className}`}>
-          {props.message.line_message_text}
+          <img src={imgUrl} />
         </div>
       );
     default:
